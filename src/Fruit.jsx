81c9e67f2f108a183d0371218902d38a2a302809
puckfried <puckfried@posteo.de>
2022-08-10 10:47:37 +0200

    solution added

---
+++ src/Fruit.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+function Fruit(){
+
+    const [fruitsState, setFruitsState] = useState([{color: 'red', fruit: 'Mango'},{color: 'red', fruit: 'Apple'}, {color: 'red', fruit: 'Banana'},{color: 'red', fruit: 'Lemon'}]);
+
+
+    function handleColor(i){
+        console.log('change color', i)
+        const newArray = fruitsState.map((el,index) => {
+                if (i===index){
+                    el.color = 'blue'
+                }     
+            return el
+            // return i===index ? {...el, color: 'blue'} : el
+         })
+        setFruitsState(newArray)
+    }
+
+
+
+    function handleDelete(i){
+        console.log('delete', i)
+        const newArray = fruitsState.filter((el,index) => {
+            if(i!==index){
+                return el
+            }
+        })
+        console.log(newArray)
+        setFruitsState(newArray)
+    }
+
+    return(
+        <>
+            <h1>Fruits</h1>
+            <ul>
+                {fruitsState.map((el,index) => {
+                    return(
+                        <li key={index}>
+                            {el.fruit}, color= {el.color} 
+                            <button onClick={() => handleColor(index)}>Change Color</button>
+                            <button onClick={() => handleDelete(index)}>Delete Me</button>
+                        </li>
+                    )
+                })}
+            </ul>
+        
+        </>
+    )
+}
+
+export default Fruit
